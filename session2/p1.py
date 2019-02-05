855745881a01362f11acc10b1645b6f8c6e34d2c
sajad1993gh <sajad1993gh@gmail.com>
2019-02-05 12:58:03 -0500

    first

---
+++ session2/p1.py
@@ -0,0 +1,9 @@
+# Eval(input()) however it crashes when it's given a string input! So always use 2 inputs to convert to r_int
+# \ continues the code to the next line!
+a = 10
+b=6
+a/b
+# donot import with .p
+# dir(p1) p1.a p1.b
+# to convert from string to int, only numeric string works. A = int(input("1"))
+# print(("Hello", end= "\n") or ..., sep = "")
