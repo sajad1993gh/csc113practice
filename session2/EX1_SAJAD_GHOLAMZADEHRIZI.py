7f3110ddeafc27db65474477ecc6cffa0b2f17bf
sajad1993gh <sajad1993gh@gmail.com>
2019-02-05 16:46:08 -0500

    more

---
+++ session2/EX1_SAJAD_GHOLAMZADEHRIZI.py
@@ -0,0 +1,27 @@
+# SAJAD_GHOLAMZADEHRIZI
+# SESSION AFTERNOON 1 _ InclassAssignment1
+import math
+
+min = 42
+sec = 42
+total_sec = (min * 60) + sec
+print(total_sec)
+
+
+r = 4.0
+volume = (4.0/3.0) * (math.pi) * (r**3)
+print(volume)
+r = 6.0
+volume = (4.0/3.0) * (math.pi) * (r**3)
+print(volume)
+
+ftemp= -40
+ctemp = (ftemp - 32) * 5/9
+print(ctemp)
+
+
+j =1
+rect_volume= j * 2*j * 3 *j
+cube_volume = j ** 3
+
+print(rect_volume/cube_volume)
