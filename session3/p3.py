7f4eb758c556818373d62dd5b0a42faeef052778
sajad1993gh <sajad1993gh@gmail.com>
2019-02-26 13:36:52 -0500

    more

---
+++ session3/p3.py
@@ -0,0 +1,22 @@
+ # Hero's Formula
+ # S = A+B+C /2
+ # Area = radical S(S-A)(S-B)(S-C)
+ # multiple assignment: 1,b,c = 1,2,3
+ def get_vertex():
+     x = float(input("enter x value"))
+     y = float(input("enter y value"))
+     return x,y
+
+
+def get_triangle():
+    x1 , y1 = get_vertex()
+    x2 , y2 = get_vertex()
+    x3, y3 = get_vertex()
+    return x1,y1,x2,y2,x3,y3
+
+def xxxx()
+def calculate_area(x1,y1,x2,y2,x3,y3)
+
+
+
+     get_vertex()
