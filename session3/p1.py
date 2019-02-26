9f8284d3e2ae8dffe1c4ee3fc7ced92039cbdd18
sajad1993gh <sajad1993gh@gmail.com>
2019-02-26 12:55:44 -0500

    more

--- session3/p1.py
+++ session3/p1.py
@@ -1,1 +1,13 @@
 #functions
+# F = celsius_to_F(ayrgument)
+
+def celsius_to_F(celsius_float):
+    """Convert Celsius to Fahrenheit. """ #documentation or docstring
+    return celsius_float*1.8 + 32;
+
+# def function_name (parameter1,parameter2):
+#        statement1
+#        statement2
+#        return value_to_return
+
+print(celsius_to_F(28));
