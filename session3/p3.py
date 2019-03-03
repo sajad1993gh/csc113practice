95d4dcd4436d459d553bd821553faf95882d1bd7
sajad1993gh <sajad1993gh@gmail.com>
2019-03-03 10:10:49 -0500

    more

--- session3/p3.py
+++ session3/p3.py
@@ -3,21 +3,34 @@
  # Area = radical S(S-A)(S-B)(S-C)
  # multiple assignment: 1,b,c = 1,2,3
  # lamda function
- def get_vertex():
-     x = float(input("enter x value"))
-     y = float(input("enter y value"))
-     return x,y
+import math
 
+def get_vertex():
+    x = float(input("    Please enter x: "))
+    y = float(input("    Please enter y: "))
+    return x,y
 
 def get_triangle():
-    x1 , y1 = get_vertex()
-    x2 , y2 = get_vertex()
-    x3, y3 = get_vertex()
-    return x1,y1,x2,y2,x3,y3
+    print("First vertex")
+    x1,y1 = get_vertex()
+    print("Second vertex")
+    x2,y2 = get_vertex()
+    print("Third vertex")
+    x3,y3 = get_vertex()
+    return x1, y1, x2, y2, x3, y3
 
-def xxxx()
-def calculate_area(x1,y1,x2,y2,x3,y3)
+def side_length(x1,y1,x2,y2):
+    '''return length of a side (Euclidean distance)'''
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
+def calculate_area(x1,y1,x2,y2,x3,y3):
+    '''return area using Heron's forumula'''
+    a = side_length(x1,y1,x2,y2)
+    b = side_length(x2,y2,x3,y3)
+    c = side_length(x3,y3,x1,y1)
+    s = (1/2)*(a + b + c)
+    return math.sqrt(s*(s-a)*(s-b)*(s-c))
 
-
-     get_vertex()
+x1, y1, x2, y2, x3, y3 = get_triangle()
+area = calculate_area(x1,y1,x2,y2,x3,y3)
+print("Area is",area)
