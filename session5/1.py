c1abc4ac654c332a1cc2f1bc7060783955778330
sajad1993gh <sajad1993gh@gmail.com>
2019-03-12 13:37:21 -0400

    more

--- session5/1.py
+++ session5/1.py
@@ -1,4 +1,4 @@
-# int("40.5") --> type error in python
+# int("40.5") --> Value error in python
 #int(float("40.5"))
 # only False and 0 are considered as False by python
 # ord("0") the order in the table
