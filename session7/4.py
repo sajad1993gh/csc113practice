bd3e9ca51fbb0d3e9b4c3296d1355f4dd378a33e
sajad1993gh <sajad1993gh@gmail.com>
2019-03-26 13:45:59 -0400

    more

--- session7/4.py
+++ session7/4.py
@@ -3,9 +3,23 @@
 sum = 0
 count = 0
 moredata = "yes"
-while moredata[0] == "y":
+while moredata[0] == "y" | moredata[0] == "Y":
     x = float(input("Enter number>> "))
     sum = sum + x
     count = count + 1
     moredata = input("more ? >> ")
 print("\n", sum/count)
+
+
+# sentinel loops
+
+xStr = input("Enter number>> "))
+
+while xStr != "":
+    x = float(xStr)
+
+
+# Nested loops:
+for i in range(10):
+    for i in range(10):
+        print(i*i)  # handle the inner loop first
