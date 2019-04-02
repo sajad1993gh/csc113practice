8025cd86a650fd591c65e0011fcf6968557f64a3
sajad1993gh <sajad1993gh@gmail.com>
2019-04-01 22:20:11 -0400

    more

--- session7/4.py
+++ session7/4.py
@@ -25,6 +25,29 @@ print("\n", sum/count)
 
 
 # Nested loops:
+print("NESTED LOOPS:\n")
 for i in range(10):
-    for i in range(10):
-        print(i*i)  # handle the inner loop first
+   for i in range(10):
+       print(i*i)  # handle the inner loop first
+
+print("NESTED LOOPS:\n")
+for i in range(10):
+   for i in range(10):
+       print(i)
+
+print("NESTED LOOPS:\n")
+for i in range(1, 5+1):
+
+   for j in range(1, i+1):
+       print("*", end=" ")
+
+   print()
+
+   """
+*
+* *
+* * *
+* * * *
+* * * * *
+
+"""
