7e7bb4a6380486a022f49a4df5a3200249fce53a
sajad1993gh <sajad1993gh@gmail.com>
2019-04-05 19:51:38 -0400

    more

--- session8/SAJAD_GHOLAMZADEHRIZI.py
+++ session8/SAJAD_GHOLAMZADEHRIZI.py
@@ -1,36 +1,56 @@
 # SAJAD_GHOLAMZADEHRIZI
 # SESSION AFTERNOON 1 _ Assignment 4
+
 import math
 # 1
 
 def perf_int(n):
-    try:
-        sum = 0
-        for x in range(1, n):
-            if n % x == 0:
-                sum += x
-        return sum == n
-    except ValueError:
-        print("Please enter valid integers")
-    except:
-        print("\nSomething went wrong, sorry!")
+    sum = 0
+    for x in range(1, n):
+        if n % x == 0:
+            sum += x
+    if sum == n:
+        return True;
+    else:
+        return False;
 
-x = int(input("Enter an integer to check if it's perfect? \n"))
-n = perf_int(x)
-if str(n) == "True":
-    print("Yes it is perfect")
-else:
-    print("No it's not perfect")
+try:
+    x = int(input("Enter an integer to check if it's perfect? \n"))
+    n = perf_int(x)
+    if str(n) == "True":
+        print("Yes it is perfect")
+    else:
+        print("No it's not perfect")
+except ValueError:
+    print("Please enter a valid integer")
+except:
+    print("\nSomething went wrong, sorry!")
 
 # 2
-
-z = int(input("Enter a positive integer to get all positive integer divisors:\n"))
-print("The divisors are:")
-for i in range(1, z+1):
-    if(z % i == 0):
-        print(i)
+moreInt = "yes"
+while moreInt[0] == "y" or moreInt[0] == "Y":
+    z = int(input("Enter a positive integer to get all positive integer divisors:\n"))
+    print("The divisors are:")
+    for i in range(1, z+1):
+        if(z % i == 0):
+            print(i)
+    moreInt = str(input("Would you like to try one more time?(yes or no)\n"))
 
 # 3
+
+def get_vertex():
+    x = float(input("    Please enter x: "))
+    y = float(input("    Please enter y: "))
+    return x,y
+
+def get_triangle():
+    print("Your First vertex")
+    x1,y1 = get_vertex()
+    print("Your Second vertex")
+    x2,y2 = get_vertex()
+    print("Your Third vertex")
+    x3,y3 = get_vertex()
+    return x1, y1, x2, y2, x3, y3
 
 def is_triangle(a, b, c):
     n = (b - c)
@@ -40,34 +60,25 @@ def is_triangle(a, b, c):
         return True
     return False
 
-def get_vertex():
-    x = float(input("    Please enter x: "))
-    y = float(input("    Please enter y: "))
-    return x,y
+try:
+    moreTriangle = "yes"
+    while moreTriangle[0] == "y" or moreTriangle[0] == "Y":
+        print("To check to see if your points make a triangle, enter your vertices")
+        x1, y1, x2, y2, x3, y3 = get_triangle()
+        a = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        b = math.sqrt((x3-x1)**2 + (y3-y1)**2)
+        c = math.sqrt((x3-x2)**2 + (y3-y2)**2)
 
-def get_triangle():
-    print("First vertex")
-    x1,y1 = get_vertex()
-    print("Second vertex")
-    x2,y2 = get_vertex()
-    print("Third vertex")
-    x3,y3 = get_vertex()
-    return x1, y1, x2, y2, x3, y3
-
-x1, y1, x2, y2, x3, y3 = get_triangle()
-
-a = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-b = math.sqrt((x3-x1)**2 + (y3-y1)**2)
-c = math.sqrt((x3-x2)**2 + (y3-y2)**2)
-
-if is_triangle(a, b, c):
-    print("Yes they form a triangle")
-else:
-    print("No they can't form a triangle")
-
-
-first = int(input("For Prime Numbers, Enter an integer to start from:\n"))
-last = int(input("Enter an  integer to end:\n"))
+        if is_triangle(a, b, c):
+            print("Yes they form a triangle")
+        else:
+            print("No they can't form a triangle")
+        moreTriangle = str(input("Do you have more points to examine? (yes or no)\n"))
+except ValueError:
+    print("Please enter a valid integer")
+except:
+    print("\nSomething went wrong, sorry!")
+# 4
 
 def is_prime(number):
     isPrimeBool = True
@@ -79,9 +90,16 @@ def is_prime(number):
                 isPrimeBool = False   #the number is divisable by any number lass than itself,then it is not prime.
     return isPrimeBool
 
-for i in range(first,last + 1):
-    if is_prime(i):
-        print(i)
+moreNum = "yes"
+while moreNum[0] == "y" or moreNum[0] == "Y":
+    first = int(input("To get the Prime Numbers, Enter an integer to start from:\n"))
+    last = int(input("Enter an  integer to end:\n"))
+    for i in range(first,last + 1):
+        if is_prime(i):
+            print(i)
+    moreNum = str(input("Do you want to try one more time? (yes or no) \n"))
+
+# 5
 
 def fact(n):
     if n == 0:
@@ -91,9 +109,10 @@ def fact(n):
         factorial = factorial * i
     return factorial
 
-
-x = int(input("For the last part, Enter the number of people you have (the value of x):\n"))
-y = int(input("Enter the number of people you want to pick and seat them around a table:\n"))
-
-answer = (fact(x)/(fact(x - y) * fact(y))) * fact(y - 1)
-print(answer)
+morePeople = "yes"
+while morePeople[0] == "y" or morePeople[0] == "Y":
+    x = int(input("Enter the number of people you have (the value of X in the Assignment):\n"))
+    y = int(input("Enter the number of people you want to pick and seat them around a table (the value of Y):\n"))
+    answer = (fact(x)/(fact(x - y) * fact(y))) * fact(y - 1)
+    print(answer)
+    morePeople = str(input("Would you like to try one more time? (yes or no)\n"))
