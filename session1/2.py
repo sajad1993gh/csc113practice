a5d9c5f849cba58f4b82e82df1105389e616094f
sajad1993gh <sajad1993gh@gmail.com>
2019-04-09 20:16:39 -0400

    more

--- session1/2.py
+++ session1/2.py
@@ -1,7 +1,14 @@
 import math
-r_str = input("Enter radius?")
+import datetime
+
+r_str = input("Enter radius?\n")
 r_int = int(r_str)
 
 circumference= 2 * math.pi * r_int
 area = math.pi * (r_int ** 2)
 print (circumference, "and", area)
+
+
+now = datetime.datetime.now()
+print("Current date and time : ")
+print(now.strftime("%Y-%m-%d %H:%M:%S\n"))
