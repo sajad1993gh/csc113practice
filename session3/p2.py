80947295f6c8c90e6e3de5e7fc5402458b17f77b
sajad1993gh <sajad1993gh@gmail.com>
2019-04-09 22:05:42 -0400

    more

--- session3/p2.py
+++ session3/p2.py
@@ -1,7 +1,7 @@
 # extract the digit from a number
 
 def digit_extraction(number,position):
-    return number//(10**position)%10
+    return (number//(10**position))%10
 
 
 print(digit_extraction(123456,5));
