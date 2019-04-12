85d5c3276fa15e767e35e1f27c6346ec61fa86d0
sajad1993gh <sajad1993gh@gmail.com>
2019-04-11 21:50:39 -0400

    more

--- session7/1-2.py
+++ session7/1-2.py
@@ -6,7 +6,7 @@ import random
 # randrange(min,max,step) modulo is optional. Return a randomly selected element from range(start, stop, step). This is equivalent to choice(range(start, stop, step)), but doesn’t actually build a range object.
 # shuffle(list) Shuffle the sequence
 # sample(list,k) Return a k length list of unique elements chosen from the list. Used for random sampling without replacement. Returns a new list containing elements from the list while leaving the original list unchanged.
-# choice(list) Return a random element from the non-empty sequence
+# choice(list) Return a random element from the non-empty list
 # list [1,"a", 3 , ...]
 # Tuple(1, "a", 3) unchangable
 a = random.randrange(4,10,2)
