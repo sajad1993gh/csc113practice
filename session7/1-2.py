6068075ef29410e83852e8aaa9591a9e98525444
sajad1993gh <sajad1993gh@gmail.com>
2019-04-11 21:37:23 -0400

    more

--- session7/1-2.py
+++ session7/1-2.py
@@ -11,25 +11,3 @@ import random
 # Tuple(1, "a", 3) unchangable
 a = random.randrange(4,10,3)
 print(a)
-
-# for <var> in <sequence>:
-#    <body>
-
-for i in [0,1,2,3]:
-    print(i)
-
-for odd in [1,3,5,7]:
-    print(odd*odd)
-
-for i in range(100): # means range(0,100) or 0 to 99. range(start,finish) finish is excluded and start is included
-    print(i)
-
-print(range(100))
-print(type(range(100))) # it is range
-
-for i in range(0,21,2): # 2 is the range step
-    print(i)
-# to go backward range(20,-1,-2) . you can't use float for none of them
-
-for i in range(20, -1, -2):
-    print(i)
