b49e931615d383039638ce4a95772e1a80c7e7f0
sajad1993gh <sajad1993gh@gmail.com>
2019-04-11 21:54:46 -0400

    more

--- session7/1.py
+++ session7/1.py
@@ -3,13 +3,13 @@ import random
 # random()
 # uniform(min,max) Return a random floating point number N such that min <= N <= max
 # randint(min,max) Return a random integer N such that min <= N <= max. Alias for randrange(min, max+1)
-# randrange(min,max,modulo) modulo is optional. Return a randomly selected element from range(start, stop, step). This is equivalent to choice(range(start, stop, step)), but doesn’t actually build a range object.
-# shuffle(list)
-# sample[list,4]
-# choice(list) returns one element randomly
+# randrange(min,max,step) modulo is optional. Return a randomly selected element from range(start, stop, step). This is equivalent to choice(range(start, stop, step)), but doesn’t actually build a range object.
+# shuffle(list) Shuffle the sequence
+# sample(list,k) Return a k length list of unique elements chosen from the list. Used for random sampling without replacement. Returns a new list containing elements from the list while leaving the original list unchanged.
+# choice(list) Return a random element from the non-empty list
 # list [1,"a", 3 , ...]
 # Tuple(1, "a", 3) unchangable
-a = random.randrange(4,10,3)
+a = random.randrange(4,10,2)
 print(a)
 
 # for <var> in <sequence>:
