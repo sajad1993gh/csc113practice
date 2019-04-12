b9a2305d54b12d4ce8943a28a94184185c992f3b
sajad1993gh <sajad1993gh@gmail.com>
2019-04-11 21:47:03 -0400

    more

--- session7/1-2.py
+++ session7/1-2.py
@@ -5,7 +5,7 @@ import random
 # randint(min,max) Return a random integer N such that min <= N <= max. Alias for randrange(min, max+1)
 # randrange(min,max,step) modulo is optional. Return a randomly selected element from range(start, stop, step). This is equivalent to choice(range(start, stop, step)), but doesn’t actually build a range object.
 # shuffle(list) Shuffle the sequence
-# sample[list,4] Return a k length list of unique elements chosen from the population sequence or set. Used for random sampling without replacement. Returns a new list containing elements from the population while leaving the original population unchanged. 
+# sample(list,k) Return a k length list of unique elements chosen from the population sequence or set. Used for random sampling without replacement. Returns a new list containing elements from the population while leaving the original population unchanged. 
 # choice(list) returns one element randomly
 # list [1,"a", 3 , ...]
 # Tuple(1, "a", 3) unchangable
