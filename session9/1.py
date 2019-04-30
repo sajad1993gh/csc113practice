90a01d868d503438ef3f06c83470a056d57fc704
sajad1993gh <sajad1993gh@gmail.com>
2019-04-30 12:55:20 -0400

    more

--- session9/1.py
+++ session9/1.py
@@ -1,12 +1,17 @@
 # lecture 7
-my_file = open("my_file.txt" , "r") #or put the address of the directory of the file_obj
+temp_file = open("temp.txt" , "w")
+#or put the address of the directory of the file_obj
 """ to check the address:
 ''' import os
     os getw
 
 
-    "r" mode is read-only mode
+    "r" mode is read-only mode and ...
     look at the table of different modes
+    the default value of file = is sys.stdout // using help.print
 """
+print("first line", file = temp_file)
+print("second line", file = temp_file)
+print("third line", file = temp_file)
 
-file_obj.close()
+temp_file.close()
