e9c51bc53b303f308b4a5f731f7071dddbfe2d44
sajad1993gh <sajad1993gh@gmail.com>
2019-04-30 12:46:12 -0400

    more

--- session9/1.py
+++ session9/1.py
@@ -1,2 +1,12 @@
 # lecture 7
-my_file = open("my_file.txt" , "r")
+my_file = open("my_file.txt" , "r") #or put the address of the directory of the file_obj
+""" to check the address:
+''' import os
+    os getw
+
+
+    "r" mode is read-only mode
+    look at the table of different modes
+"""
+
+file_obj.close()
