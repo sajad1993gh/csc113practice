ad3887e86928d3fbd075500493009c482f47263c
sajad1993gh <sajad1993gh@gmail.com>
2019-04-30 13:04:21 -0400

    more

---
+++ session9/2.py
@@ -0,0 +1,10 @@
+""" file.read for reading from a file
+"""
+
+phonebook = open("phonebook.txt", "r")
+
+phonebook.read()
+
+""" doing another time, it reads from the last point on """
+
+phonebook.read()
