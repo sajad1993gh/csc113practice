45ed917b3bf0a4211676aba210da47893bc745c0
sajad1993gh <sajad1993gh@gmail.com>
2019-04-30 13:33:27 -0400

    more

--- session9/2.py
+++ session9/2.py
@@ -18,10 +18,20 @@ print(phonebook.readline())
 print(phonebook.readline())  this prints empty string
 """
 
-print(phonebook.readlines())
+#print(phonebook.readlines())
 
 """
 phonebook.seek(0) sets the tracker at 0'th character
+to read that do phonebook.readline()
 phonebook.tell() tells you where your tracker is
 """
-print(phonebook.seek(20))
+#making changes in the files:
+with open("phonebook.txt", "a") as f:
+    f.write("\nSajad: 3456789")
+"""
+what is you wanna add stuff to the beginning of the file without changing the stuff in it:
+
+with open("phonebook.txt", "r+") as f:
+    data1 = f.read() # saving it in data1
+    f.seek(0)
+    f.write("\nSajad: 3456789" + data1) # overwriting it 
