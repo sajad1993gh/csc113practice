1af1998431e2a1a7886ec2ee268d3b6f316514f3
sajad1993gh <sajad1993gh@gmail.com>
2019-04-30 13:11:27 -0400

    more

--- session9/2.py
+++ session9/2.py
@@ -3,8 +3,15 @@
 
 phonebook = open("phonebook.txt", "r")
 
-phonebook.read()
+#print(phonebook.read())
+
 
 """ doing another time, it reads from the last point on """
 
-phonebook.read()
+#print(phonebook.read())
+
+print(phonebook.readline())
+print(phonebook.readline())
+print(phonebook.readline())
+
+print(phonebook.readline()) # this prints empty string
