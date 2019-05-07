982e677545118edf9e1cd87a2bc27fbcd7597a50
sajad1993gh <sajad1993gh@gmail.com>
2019-05-07 12:45:42 -0400

    more

--- session10/1.py
+++ session10/1.py
@@ -1,2 +1,9 @@
 # class
 # list (arrays)
+# list("Hello")
+# slicing ([startpoint : endpoint])
+# membership ( ... in ... operator returning true or false)
+# len
+# [1,2,3] + [4] = [1,2,3,4]
+# 1 in [1,2,3] --> True
+# [1,2,3] < [1,2] --> False . cuz anything is greater than NULL element
