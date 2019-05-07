081ed80448a5ef28a8d9cee87f4b06e626ef08a1
sajad1993gh <sajad1993gh@gmail.com>
2019-05-07 12:48:59 -0400

    more

--- session10/1.py
+++ session10/1.py
@@ -1,9 +1,12 @@
 # class
 # list (arrays)
 # list("Hello")
-# slicing ([startpoint : endpoint])
+# slicing (mylist[startpoint : endpoint])
 # membership ( ... in ... operator returning true or false)
 # len
 # [1,2,3] + [4] = [1,2,3,4]
 # 1 in [1,2,3] --> True
 # [1,2,3] < [1,2] --> False . cuz anything is greater than NULL element
+# indexing is 0 ..... 3
+# 4 .... -1
+# pay attention to return data type pf myList[:3] which is slicing which is a list
